3fbea3623d12b885bf902b80402ab58eeb6dcfd7
hatsumi3 <al.usamiku39@gmail.com>
2021-10-16 15:21:52 +0900

    initial commit

---
+++ src/modules/TestComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p class="border__blue">this is test.</p>
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponent",
+  data() {
+    return {
+      testData: "test data",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div {
+  color: red;
+}
+
+.border {
+  &__blue {
+    border: 1px blue soild;
+    color: blue;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <TestComponent />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TestComponent from "@/modules/TestComponent.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    TestComponent,
   },
 };
 </script>
